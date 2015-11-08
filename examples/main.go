7a026ea6891f37b50bb76b40fc4f67c43fc019a4
Bjørn Fjukstad <bjorn@cs.uit.no>
2015-11-08 16:16:24 +0100

    add get projects (search for tag)

--- examples/main.go
+++ examples/main.go
@@ -11,4 +11,14 @@ func main() {
 	p, err := scratch.GetProject(id)
 
 	fmt.Println(p, err)
+
+	tag := "kodeklubbentromso"
+
+	projects, err := scratch.GetProjects(tag)
+
+	for _, p := range projects {
+		fmt.Println(p)
+	}
+
+	fmt.Println(projects, err)
 }
--- scratch.go
+++ scratch.go
@@ -4,7 +4,26 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
+	"strings"
 )
+
+type ProjectSearch struct {
+	Fields   Fields `json:"fields"`
+	ImageURL string `json:"image_url"`
+	Model    string `json:"model"`
+	Pk       int    `json:"pk"`
+}
+
+type Fields struct {
+	Creator              Creator `json:"creator"`
+	FavoriteCount        int     `json:"favorite_count"`
+	LoveCount            int     `json:"love_count"`
+	RemixersCount        int     `json:"remixers_count"`
+	Thumbnail            string  `json:"thumbnail"`
+	Title                string  `json:"title"`
+	UncachedThumbnailURL string  `json:"uncached_thumbnail_url"`
+	ViewCount            int     `json:"view_count"`
+}
 
 type Project struct {
 	Creator        Creator `json:"creator"`
@@ -20,8 +39,11 @@ type Project struct {
 }
 
 type Creator struct {
-	Username    string      `json:"username"`
-	Userprofile Userprofile `json:"userprofile"`
+	Username     string      `json:"username"`
+	Userprofile  Userprofile `json:"userprofile"`
+	Admin        bool        `json:"admin"`
+	Pk           int         `json:"pk"`
+	ThumbnailURL string      `json:"thumbnail_url"`
 }
 
 type Userprofile struct {
@@ -47,6 +69,43 @@ func GetProject(id string) (*Project, error) {
 		return nil, err
 	}
 	return p, nil
+}
+
+func GetProjects(tag string) ([]*Project, error) {
+
+	url := "https://scratch.mit.edu/site-api/explore/projects/" + tag + "/?date=this_month"
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	ps := make([]ProjectSearch, 0)
+	err = json.Unmarshal(body, &ps)
+	if err != nil {
+		return nil, err
+	}
+
+	projects := []*Project{}
+
+	for _, project := range ps {
+		id := project.ImageURL
+		id = strings.TrimPrefix(id, "//cdn2.scratch.mit.edu/get_image/project/")
+		id = strings.Split(id, "_")[0]
+
+		p, err := GetProject(id)
+		if err != nil {
+			return nil, err
+		}
+		projects = append(projects, p)
+	}
+
+	return projects, nil
+
 }
 
 //<img src="//cdn2.scratch.mit.edu/get_image/project/86658772_216x163.png?v=1446739374.02" class="image">ck
